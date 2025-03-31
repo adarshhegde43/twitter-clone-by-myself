99b105d3d8e85dc5971f2f852b4ce067c04478fc
Adarsh Hegde <ch22b048@smail.iitm.ac.in>
2025-03-31 12:07:31 +0530

    1 - user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express" ;//express is a module already downloaded on your desktop..
-import authRoutes from "./routes/auth.routes.js"; //Don't forget to add .js at the end , because we are using type:module...
 import dotenv from "dotenv" ;
-import connectMongoDb from "./dB/connectMongoDb.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary} from "cloudinary";
+
+import authRoutes from "./routes/auth.routes.js"; //Don't forget to add .js at the end , because we are using type:module...
+import userRoutes from "./routes/user.routes.js";
+
+import connectMongoDb from "./dB/connectMongoDb.js";
 
 dotenv.config() ; //allows us to access process.env...
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY ,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express(); //Function which creates express app...
 const PORT = process.env.PORT || 5000 ;
@@ -14,6 +23,7 @@ app.use(express.urlencoded({extended : true})) ; //to parse form-data (urlencode
 app.use(cookieParser()); //parse cookies...
 
 app.use("/api/auth" , authRoutes);
+app.use("/api/users" , userRoutes);
 
 app.listen(PORT , (req,res) => {
     console.log(`You are listening to port ${PORT}`);
